637f66202c798d902bfa82f4a913c47fba70ff7b
Joao Pedro <joaopedrot1912@gmail.com>
2025-09-13 17:07:35 -0300

    feat: first commit, app health

---
+++ tests/index.test.ts
@@ -0,0 +1,20 @@
+import request from "supertest";
+import app from "../src";
+import { Server } from "http";
+
+let server: Server;
+
+beforeAll(() => {
+  server = app.listen(); 
+});
+
+afterAll((done) => {
+  server.close(done);
+});
+
+describe("GET /", () => {
+  it("deve retornar Hello World", async () => {
+    const res = await request(server).get("/");
+    expect(res.text).toBe("Hello World");
+  });
+});
