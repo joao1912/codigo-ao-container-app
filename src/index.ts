2697309469fffbfa0025294618fe4aab0e583671
Joao Pedro <joaopedrot1912@gmail.com>
2025-09-15 21:01:42 -0300

    refactor: new port

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,8 @@ app.get("/", (__: Request, res: Response) => {
   res.send("Hello World");
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(80, () => {
+  console.log(`Server running on http://localhost`);
 });
 
 export default app
